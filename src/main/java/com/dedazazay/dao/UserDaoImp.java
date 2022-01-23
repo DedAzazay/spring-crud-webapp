230db83e9038c87da652b0924cf21e34a2301959
Dmitry Mishakov <welzevul0@gmail.com>
2022-01-23 22:43:17 +0300

    Realized Dao class with EntityManager.

--- src/main/java/com/dedazazay/dao/UserDaoImp.java
+++ src/main/java/com/dedazazay/dao/UserDaoImp.java
@@ -1,34 +1,46 @@
 package com.dedazazay.dao;
 
 import com.dedazazay.model.User;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
+@Component
 public class UserDaoImp implements UserDao{
 
-
+    @PersistenceContext
+    public EntityManager em;
+    
     @Override
     public List<User> list() {
-        return null;
+        TypedQuery<User> namedQuery = em.createNamedQuery("User.getAll", User.class);
+        return namedQuery.getResultList();
     }
 
     @Override
     public User show(int id) {
-        return null;
+        return em.find(User.class, id);
     }
 
     @Override
+    @Transactional
     public void save(User user) {
-
+        em.merge(user);
     }
 
     @Override
+    @Transactional
     public void update(int id, User updatedUser) {
-
+        em.merge(updatedUser);
     }
 
     @Override
+    @Transactional
     public void delete(int id) {
-
+        em.remove(show(id));
     }
 }
