f72bf21b05e7e0a839360469e00764c2c5227df6
Dmitry Mishakov <welzevul0@gmail.com>
2022-01-23 22:42:26 +0300

    Realized Controller with userDao class.

--- src/main/java/com/dedazazay/controllers/UserController.java
+++ src/main/java/com/dedazazay/controllers/UserController.java
@@ -1,44 +1,72 @@
 package com.dedazazay.controllers;
 
+import com.dedazazay.dao.UserDao;
+import com.dedazazay.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserDao userDao;
+
+    @Autowired
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @GetMapping()
-    public String allUsers() {
+    public String allUsers(Model model) {
+        model.addAttribute("users", userDao.list());
         return "/list";
     }
 
     @GetMapping("/{id}")
-    public String oneUser(int id) {
+    public String oneUser(@PathVariable("id") int id, Model model) {
+        model.addAttribute("users", userDao.show(id));
         return "/show";
     }
 
     @GetMapping("/new")
-    public String newUser(/* User user */) {
+    public String newUser(@ModelAttribute("user") User user) {
         return "/new";
     }
 
     @PostMapping()
-    public String createUser(/* User user */) {
+    public String createUser(@ModelAttribute("user") @Valid User user,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/new";
+        }
+        userDao.save(user);
         return "redirect:/users";
     }
 
     @GetMapping("/{id}/edit")
-    public String editUser(/* fields of User */) {
+    public String editUser(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userDao.show(id));
         return "/edit";
     }
 
     @PatchMapping("/{id}")
-    public String updateUser(/* some fields */) {
+    public String updateUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+                             @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()){
+            return "/edit";
+        }
+        userDao.update(id, user);
         return "redirect:/users";
     }
 
     @DeleteMapping("/{id}")
-    public String deleteUser() {
+    public String deleteUser(@PathVariable("id") int id) {
+        userDao.delete(id);
         return "redirect:/users";
     }
 }
